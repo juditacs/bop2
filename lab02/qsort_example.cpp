756f2207ea0bbd6ad44dfbc6d4c79d92e3604a27
Judit Acs <judit@sch.bme.hu>
2020-02-19 18:47:00 +0100

    add dynamic memory allocation

--- lab02/qsort_example.cpp
+++ lab02/qsort_example.cpp
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 #define ARRAY_MAX 5
+
 // The compare function compares two values received as parameters.
 // We have to specify this function with general parameter types, since
 // being passed to the qsort, such approach insures the type safety.
@@ -10,24 +11,29 @@ int compare(const void* a, const void *b)
 	// Returning value: <0, if a<b;
 	// 0, if a=b;
 	// >0, if a>b.
-	double* da=(double*)a, *db=(double*)b;
-	if(*da<*db) return-1;
-	else if (*da==*db) return 0;
-	else return 1;
+    double* da=(double*)a, *db=(double*)b;
+    if(*da<*db) return-1;
+    else if (*da==*db) return 0;
+    else return 1;
 }
+
 int main()
 {
-	double d[ARRAY_MAX];
-	int i;
-	for(i=0;i<ARRAY_MAX;i++)
+    int arraySize;
+    std::cout << "How many numbers should I expect?\n";
+    std::cin >> arraySize;
+	double* d = (double*)malloc(sizeof(double)*arraySize);
+	for(int i=0; i<arraySize; i++)
 	{
-		printf("Enter a number:");
-		scanf("%lf", &d[i]);
+        std::cout << "Enter a number:";
+        std::cin >> d[i];
 	}
-	qsort(d,ARRAY_MAX,sizeof(double), compare);
-	for(i=0;i<ARRAY_MAX;i++)
+	qsort(d, arraySize, sizeof(double), compare);
+	for(int i=0; i<arraySize; i++)
 	{
-		printf("%lf ",d[i]);
+        std::cout << d[i] << " ";
 	}
+    std::cout << std::endl;
+    free(d);
 	return 0;
 }
