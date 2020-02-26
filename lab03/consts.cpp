16ad8a0b27f27269d2cc252822a7cb469960323e
Judit Acs <judit@sch.bme.hu>
2020-02-26 15:42:03 +0100

    consts int starter code

---
+++ lab03/consts.cpp
@@ -0,0 +1,50 @@
+/*
+ * consts.cpp
+ * Copyright (C) 2020 Judit Acs <judit@sch.bme.hu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <iostream>
+
+int main() {
+	// 1.
+	const int i;
+    /*
+	// 2.
+	const int i=90;
+	i++;
+	// 3.
+	const int i=90;
+	int*p=&i;
+	(*p)++;
+	// 4.
+	const int v[]={1,2,3};
+	v[1]++;
+	// 5.
+	const int i=255;
+	intv[i];
+	// 6.
+	char s[]="Hello";
+	const char*pc=s;
+	pc[0]='h';
+	pc++;
+	// 7.
+	char s[]="Hello";
+	char* const cp=s;
+	cp[0]='h';
+	cp++;
+	// 8.
+	char s[]="Hello";
+	const char* const cpc=s;
+	cpc[0]='h';
+	cpc++;
+	//9.
+	int j = 0;
+	int const &i = j;
+	i = 1;
+	const int& i = j;
+	i = 1;  
+    */
+	return 0;
+}
