1bb3a52e03b460487118a38d6c48fb623ccf798d
Judit Acs <judit@sch.bme.hu>
2020-03-04 09:42:21 +0100

    add sanity check

--- lab04/main.cpp
+++ lab04/main.cpp
@@ -6,6 +6,9 @@ int main() {
     std::cout << "Enter a rectangle's two sides:\n";
     std::cin >> a >> b;
     Rectangle r1 = Rectangle(a, b);
+    // try uncommenting this line. It should produce a compile error, 
+    // the attribute should not be accessible from the outside.
+    // r1.a;
     std::cout << "Information about the rectangle:\n";
     r1.print();
     return 0;
